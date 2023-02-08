1b845663158993bfec150eb7238bb3397b9aa4a3
Abhishek Singh <ss738556@gmail.com>
2023-02-08 13:45:26 +0530

    code change

---
+++ dice_simulater/project1.py
@@ -0,0 +1,13 @@
+# Dice rolling simulater
+
+# import random
+# min_val=1
+# max_val=6
+
+# start_rolling="y"
+# while start_rolling=="y":
+#     print("lets roll the dice")
+#     print("the value is:")
+#     d=random.randint(1,6)
+#     print(d)
+#     start_rolling=input("want to roll dice again")
